56baadfcfd42536e0a5f3f8701ca67c57b79d226
Vladimir <110543788+ShkerbsKababik@users.noreply.github.com>
2024-12-05 15:49:48 +0300

    commit1

---
+++ DartsApp.Client/DartsApp.Client/Services/CookieService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.JSInterop;
+
+namespace DartsApp.Client.Services
+{
+    public interface ICookieService
+    {
+        public Task SetCookieAsync(string name, string value);
+        public Task<string> GetCookieAsync(string name);
+        public Task DeleteCookieAsync(string name);
+    }
+    public class CookieService : ICookieService
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public CookieService(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public async Task SetCookieAsync(string name, string value)
+        {
+            await _jsRuntime.InvokeVoidAsync("setCookie", name, value);
+        }
+
+        public async Task<string> GetCookieAsync(string name)
+        {
+            string str = await _jsRuntime.InvokeAsync<string>("getCookie", name);
+            return str;
+        }
+
+        public async Task DeleteCookieAsync(string name)
+        {
+            await _jsRuntime.InvokeVoidAsync("deleteCookie", name);
+        }
+    }
+}

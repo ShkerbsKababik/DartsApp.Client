1104ec2e3b7c4cba9f172af47ef71b8b72ac2be3
Vladimir <110543788+ShkerbsKababik@users.noreply.github.com>
2024-12-20 16:32:28 +0300

    commit2

--- DartsApp.Client/DartsApp.Client/Program.cs
+++ DartsApp.Client/DartsApp.Client/Program.cs
@@ -7,10 +7,17 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// add HttpClient
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp =>
+    new HttpClient
+    {
+        BaseAddress = new Uri("http://localhost:5213")
+    });
+
 builder.Services.AddScoped<IUserSessionProvider, OfflineUserSessionProvider>();
 builder.Services.AddScoped<ICookieService, CookieService>();
-builder.Services.AddScoped<IGameSessionHandler, GameSessionHandler>();
+builder.Services.AddScoped<IGameService, GameService>();
 
 await builder.Build().RunAsync();
 
---
+++ DartsApp.Client/DartsApp.Client/Services/GameService.cs
@@ -0,0 +1,81 @@
+﻿using System.Net.Http;
+using System;
+using System.Net.Http.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DartsApp.Client.Services
+{
+    public interface IGameService
+    {
+        public Guid CreateGame(GameCreationInfo creationInfo);
+        public void ApplyScore(ApplyScoreInfo scoreInfo);
+        public Game GetGameInfo(Guid gameId);
+    }
+    public class GameService : IGameService
+    {
+        private readonly HttpClient _httpClient;    
+        public GameService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public void ApplyScore(ApplyScoreInfo scoreInfo)
+            => ApplyScoreAsync(scoreInfo);
+
+        public async void ApplyScoreAsync(ApplyScoreInfo scoreInfo)
+        {
+            await _httpClient.PostAsJsonAsync("https://localhost:7030/GameService/ApplyScore", scoreInfo);
+        }
+
+        public Guid CreateGame(GameCreationInfo creationInfo)
+            => CreateGameAsync(creationInfo).Result;
+
+        public async Task<Guid> CreateGameAsync(GameCreationInfo creationInfo)
+        {
+            var response = await _httpClient.PostAsJsonAsync("https://localhost:7030/GameService/CreateGame", creationInfo);
+            return await response.Content.ReadFromJsonAsync<Guid>();
+        }
+
+        public Game GetGameInfo(Guid gameId)
+            => GetGameInfoAsync(gameId).Result;
+
+        public async Task<Game> GetGameInfoAsync(Guid gameId)
+        {
+            var response = await _httpClient.PostAsJsonAsync("https://localhost:7030/GameService/GetGameInfoAsync", gameId);
+           return await response.Content.ReadFromJsonAsync<Game>();      
+        }
+    }
+    public class GameCreationInfo
+    {
+        public Guid OwnerId { get; set; }
+        public List<Guid> Players { get; set; }
+    }
+    public class ApplyScoreInfo
+    {
+        public Guid GameId { get; set; }
+        public Guid PlayerId { get; set; }
+
+        public int FirstRun { get; set; }
+        public int SecondRun { get; set; }
+        public int ThirdRun { get; set; }
+    }
+    public class Game
+    {
+        public Guid Id { get; set; }
+        public Guid Owner { get; set; }
+        public Guid CurrentPlayer { get; set; }
+        public GameStatus Status { get; set; }
+
+        public DateTime CreationTime { get; set; }
+        public DateTime? EndTime { get; set; }
+
+        public Guid[] Players { get; set; }
+        public Guid[] Scores { get; set; }
+    }
+    public enum GameStatus
+    {
+        InProcess,
+        Canceled,
+        Ended
+    }
+}
